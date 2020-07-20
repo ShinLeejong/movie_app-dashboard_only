d5045e57d5730823ab462c831244ab05b8a333b9
iiy4383 <znlspf123@naver.com>
2020-07-20 21:57:48 +0900

    2020/07/20

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
+function FirstComponent(props) {
+  return props.whatdoyoulove;
+}
+
 function App() {
-  return <div className="App" />;
+  return(
+    <div>
+      <h3>
+      hi! this is from App.js.
+      </h3>
+      <h3>
+      I like <FirstComponent whatdoyoulove="kimchi" />.
+      </h3>
+    </div>
+  )
 }
 
 export default App;
